045ed51224e0bbcec66bf75ccd435ab15e9b4c0f
pmarper0610 <pmarper0610@g.educaand.es>
2025-01-09 12:18:24 +0100

    blink2 creado (con el nombre "millis")

---
+++ blink.py
@@ -0,0 +1,28 @@
+import pygame as pg
+import time
+
+funcionando = True     #Esta variable es para parar pygame cuando terminemos
+estado = False         #Esta es para ir cambiando el color del círculo
+
+pg.init()
+screen = pg.display.set_mode((500,500),0,32)
+
+while funcionando:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            funcionando = False 
+            break
+
+    if estado:
+        color = [250,250,0]   
+        estado = False
+    else:
+        color = [50,50,50] 
+        estado = True
+    screen.fill([100,100,100]) 
+    pg.draw.circle(screen,color,(200,200),70)
+    pg.display.flip()
+
+    time.sleep(1)
+
+pg.quit() 
