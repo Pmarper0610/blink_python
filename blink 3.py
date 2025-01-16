27bf0633fd2a390e3454bd1262dbc57afcf1eb20
pmarper0610 <pmarper0610@g.educaand.es>
2025-01-16 12:31:11 +0100

    trabajo dia 16 jueves

--- blink 3.py
+++ blink 3.py
@@ -1,28 +1,34 @@
 import pygame as pg
-import time
+from threading import Timer
 
 funcionando = True     #Esta variable es para parar pygame cuando terminemos
 estado = False         #Esta es para ir cambiando el color del círculo
 color = [(220,220,0), (50,50,50)]
 pg.init()
 screen = pg.display.set_mode((500,500),0,32)
-tiempoInicial = time.process_time()*1000
+
+def blink():
+    global estado
+    global t 
+    estado = not(estado)
+    t = Timer(1, blink)
+    t.start ()
+
+t = Timer(1, blink)
+t.start()
+
 
 while funcionando:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             funcionando = False 
             break
-    tiempoActual = time.process_time()*1000
-    if (tiempoActual - tiempoInicial) > 200:
-        
-        tiempoInicial = time.process_time()*1000
-        estado = not(estado)
+   
 
     screen.fill([100,100,100]) 
    
   
-    pg.draw.circle(screen,color[estado]             ,(200,200),70) 
+    pg.draw.circle(screen,color[estado],(200,200),70) 
     pg.display.flip()
 
 
--- blink4.py
+++ blink4.py
@@ -1,29 +1,34 @@
 import pygame as pg
+from timer_event import TimerEvent
 import time
 
+contador = 1
 funcionando = True     #Esta variable es para parar pygame cuando terminemos
 estado = False         #Esta es para ir cambiando el color del círculo
 color = [(220,220,0), (50,50,50)]
 pg.init()
 screen = pg.display.set_mode((500,500),0,32)
-tiempoInicial = time.process_time()*1000
+
+def blink(packet):
+    global estado 
+    estado = not(estado)
+    
+te = TimerEvent(interval=1.0)
+te.subscribe("Temporizador1_handler" , blink)
+
 
 while funcionando:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             funcionando = False 
             break
-    tiempoActual = time.process_time()*1000
-    if (tiempoActual - tiempoInicial) > 200:
-        
-        tiempoInicial = time.process_time()*1000
-        estado = not(estado)
+   
 
     screen.fill([100,100,100]) 
-   
-  
-    pg.draw.circle(screen,color[estado]             ,(200,200),70) 
+    pg.draw.circle(screen,color[estado],(200,200),70) 
     pg.display.flip()
 
+    print(contador, end='/r')
+    contador = contador
 
 pg.quit() 
